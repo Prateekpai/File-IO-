021873e7da8bc3ce98b2e4669ebca951b1a70de0
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-04 08:41:00 +0530

    Read or Write File using File IO

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+namespace FileIODemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"E:\LFP 119\Day 27\UC13\UC13\AddressBook.txt";
+            ReadAllLinesFromFile(path);
+            ReadTextFile(path);
+
+        }
+
+        public static void ReadAllLinesFromFile(string path)
+        {
+            string[] Line;
+
+            Line = File.ReadAllLines(path);
+
+            Console.WriteLine(Line[0]);
+        }
+
+        public static void ReadTextFile(string path)
+        {
+            string Line;
+
+            Line = File.ReadAllText(path);
+
+            Console.WriteLine(Line);
+        }
+
+    }
+}
