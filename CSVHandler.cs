547a2abd819fabbe0ecd0b4262ae7d97355c776a
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-04 22:26:00 +0530

    Read or Write Address Book As JSON File

---
+++ CSVHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace JSONDemo
+{
+    public class JSONHandler
+    {
+        public static void JSONImplement()
+        {
+            string importJSONFilePath = @"E:\LFP 119\Day 28\UC15\UC15\Import.json";
+
+            string ExportJSOnFilePath = @"E:\LFP 119\Day 28\UC15\UC15\Export.json";
+
+            var Obj = JsonConvert.DeserializeObject<List<AddressData>>(File.ReadAllText(importJSONFilePath));
+
+            JsonSerializer serializer = new JsonSerializer();
+            using (StreamWriter sw = new StreamWriter(ExportJSOnFilePath))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, Obj);
+            }
+        }
+    }
+}
